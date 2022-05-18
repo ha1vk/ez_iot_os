502d54ca075f53aed55a6e4ac316d05922fe91e0
xurongjun <xurongjun@ezvizlife.com>
2022-05-18 19:30:09 +0800

    [mod] 更新静态代码检测默认构建脚本

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-cd ./apps/ezapp_template/port/$1
+LOC_PWD=${PWD}
+
+cd ${LOC_PWD}/apps/ezapp_template/port/$1
+mkdir build
+cd ./build
+cmake ..
+make -j8
+
+cd ${LOC_PWD}/examples
 mkdir build
 cd ./build
 cmake ..
